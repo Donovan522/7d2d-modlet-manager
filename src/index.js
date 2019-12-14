d1b60e96c7b714004a306f4b66fd835f5947a03a
Donovan C. Young <dyoung522@gmail.com>
2019-12-14 02:41:55 -0500

    code cleanup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import unhandled from "electron-unhandled";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 unhandled();
 
 ReactDOM.render(<App config={new Store({ name: "7d2dmm" })} />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
